06bc3b9f262867acf001b8dfa1877d5144d8e11f
emrahelis <emrahelis@gmail.com>
2024-10-12 11:46:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Bir değer giriniz");
+try
+{
+    int value = Cevir(Console.ReadLine());
+    int result = value / 0;
+}
+catch (Exception ex)
+{
+
+
+}
+
+
+static int Cevir(string deger)
+{
+
+    int value = 0;
+    try
+    {
+        value = int.Parse(deger);
+    }
+    catch (Exception ex)
+    {
+        //throw ex;
+        throw new UnauthorizedAccessException(ex.Message);
+    }
+
+    return value;
+
+}
