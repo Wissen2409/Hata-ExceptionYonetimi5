196e3e5777802e7c2a41ea1c63debac5bd07b71c
emrahelis <emrahelis@gmail.com>
2024-10-12 11:46:57 +0300

    kmk

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
 ﻿
+using System.Net.Http.Headers;
+
 Console.WriteLine("Bir değer giriniz");
 try
 {
     int value = Cevir(Console.ReadLine());
     int result = value / 0;
 }
+catch(DivideByZeroException ex){
+
+// dıştaki try catch'i yakalar
+}
+catch(UnauthorizedAccessException ex){
+
+    // içteki try catch 'i yakalar
+}
 catch (Exception ex)
 {
 
+    // yukarıdakilerin yakalayamadığı bir hata olursa, bu blok yakalayacaktır
 
 }
-
-
 static int Cevir(string deger)
 {
-
     int value = 0;
     try
     {
